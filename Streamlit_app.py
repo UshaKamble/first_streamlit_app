96914330154701b29288ad0c94d3f61aad707926
UshaKamble <134099418+UshaKamble@users.noreply.github.com>
2023-05-20 12:02:27 +0530

    Update Streamlit_app.py
    Added new menu

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Aditya Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🍞 Aditya Special Sandwitch')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
