14288509269a5a414011e4f12ef246ed022b2d4f
UshaKamble <134099418+UshaKamble@users.noreply.github.com>
2023-05-20 12:08:04 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.text('🍞 Aditya Special Sandwitch')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 Import Pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
